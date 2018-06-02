8082d8604b377f5481bfa3d3a90224521b83aa99
Kus Cámara <kcmr@users.noreply.github.com>
2018-06-03 00:41:34 +0200

    chore(tasks): use polymer serve as proxied server in browserSync

--- tasks/server.js
+++ tasks/server.js
@@ -1,15 +1,43 @@
 'use strict';
 
 const browserSync = require('browser-sync');
+const portfinder = require('portfinder');
+const { spawn } = require('child_process');
 const config = require('./config');
 const manifest = require('../package.json');
-const component = manifest.name;
-const serverConfig = JSON.parse(
-  JSON.stringify(config.server)
-    .replace(/{{component}}/g, component)
-  );
 
-browserSync
-  .init(serverConfig)
-  .watch(config.watch.sources)
-  .on('change', browserSync.reload);
+const startServer = (port) => {
+  const polymerCliParams = ['serve', '--npm', '-p'];
+  const polymerServe = spawn('polymer', polymerCliParams.concat(port));
+
+  polymerServe.stdout.on('data', (data) => {
+    console.log(`${data}`);
+
+    browserSync
+      .init(getServerConfig(port))
+      .watch(config.watch.sources)
+      .on('change', browserSync.reload);
+  });
+
+  polymerServe.stderr.on('data', (data) => {
+    console.log(`Error: ${data}`);
+  });
+
+  polymerServe.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+const getServerConfig = (port) => {
+  const component = manifest.name;
+  return JSON.parse(
+    JSON.stringify(config.server)
+      .replace(/{{component}}/g, component)
+      .replace(/{{port}}/g, port)
+    );
+};
+
+portfinder.getPortPromise()
+  .then(startServer)
+  .catch((err) => console.log(err));
+

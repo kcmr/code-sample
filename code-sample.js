09e5c0791b66099982385fc98d86408cc9de21db
hackyoung <hackyoung@163.com>
2018-08-02 13:17:51 +0800

    修改

--- code-sample.js
+++ code-sample.js
@@ -1,8 +1,7 @@
-import { LitElement, html } from '../../@polymer/lit-element/lit-element.js';
-import { FlattenedNodesObserver } from '../../@polymer/polymer/lib/utils/flattened-nodes-observer.js';
-import { styles } from './styles.js';
-import { oneDark } from './themes/one-dark.js';
-import '../../highlightjs/highlight.pack.min.js';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+import { Templatizer } from '@polymer/polymer/lib/legacy/templatizer-behavior.js';
+import 'highlightjs/highlight.pack.min.js';
 
 /* global hljs */
 
@@ -12,36 +11,83 @@ import '../../highlightjs/highlight.pack.min.js';
  * @customElement
  * @demo https://kcmr.github.io/code-sample/
  */
-class CodeSample extends LitElement {
-  _render({copyClipboardButton}) {
+class CodeSample extends PolymerElement {
+
+  static get template() {
     return html`
-      ${styles}
-      ${this.theme || oneDark}
+      <style inlcude="[[theme]]">
+        :host {
+          display: block;
+        }
+        :host([hidden]),
+        [hidden] {
+          display: none;
+        }
+        pre {
+          margin: 0;
+        }
+        pre, code {
+          font-family: var(--code-sample-font-family, Operator Mono, Inconsolata, Roboto Mono, monaco, consolas, monospace);
+          font-size: var(--code-sample-font-size, 14px);
+        }
+        .hljs {
+          padding: 0 20px;
+          line-height: 1.3;
+        }
+        .demo:not(:empty) {
+          padding: var(--code-sample-demo-padding, 0 0 20px);
+        }
+        .demo {
+          @apply --code-sample-demo;
+        }
+        #code-container {
+          position: relative;
+          @apply --code-sample-code-container;
+        }
+        #code-container:hover {
+          @apply --code-sample-code-container-hover;
+        }
+        #code-container:hover > button {
+          @apply --code-sample-code-container-hover-button;
+        }
+        button {
+          background: #e0e0e0;
+          border: none;
+          cursor: pointer;
+          display: block;
+          visibility: hidden;
+          position: absolute;
+          right: 0;
+          top: 0;
+          text-transform: uppercase;
+          @apply --code-sample-copy-clipboard-button;
+        }
+        :host([copy-clipboard-button]) button {
+          visibility: visible;
+        }
+      </style>
+      <div id="theme"></div>
       <div id="demo" class="demo"></div>
 
       <slot id="content"></slot>
 
       <div id="code-container">
         <button id="copy-button"
-          hidden?="${!copyClipboardButton}"
+          id="copyButton"
           title="Copy to clipboard"
-          on-click="${() => this._copyToClipboard()}">Copy</button>
+          on-click="_copyToClipboard">Copy</button>
         <pre id="code"></pre>
       </div>
     `;
   }
 
-  _firstRendered() {
-    this.$_content = this.shadowRoot.querySelector('#content');
-    this.$_code = this.shadowRoot.querySelector('#code');
-    this.$_copyButton = this.shadowRoot.querySelector('#copy-button');
-    this.$_demo = this.shadowRoot.querySelector('#demo');
-  }
-
   static get properties() {
     return {
       // Set to true to show a copy to clipboard button.
-      copyClipboardButton: Boolean,
+      copyClipboardButton: {
+        type: Boolean,
+        reflectToAttribute: true
+      },
       // Tagged template literal with custom styles.
       theme: String,
       // Set to true to render the code inside the template.
@@ -49,23 +95,14 @@ class CodeSample extends LitElement {
       // Code type (optional). (eg.: html, js, css)
       // Options are the same as the available classes for `<code>` tag using highlight.js
       type: String,
+      // theme name
+      themeName: {
+        type: String,
+        reflectToAttribute: true,
+        observer: '_themeNameChanged',
+        value: 'one-dark',
+      }
     };
-  }
-
-  static get observedAttributes() {
-    return [
-      'copy-clipboard-button',
-      'render',
-      'type',
-      'theme',
-    ];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    const propName = this._toCamelCase(name);
-    this[propName] = (this.constructor.properties[propName] === Boolean)
-      ? this.hasAttribute(name)
-      : newValue;
   }
 
   _toCamelCase(value) {
@@ -74,49 +111,77 @@ class CodeSample extends LitElement {
     );
   }
 
+  _themeNameChanged(themeName) {
+    let imported = null;
+    switch (themeName) {
+    case 'atom-one-light':
+      imported = import('./themes/atom-one-light.js');
+      break;
+    case 'one-dark':
+      imported = import('./themes/one-dark.js');
+      break;
+    case 'default':
+      imported = import('./themes/default.js');
+      break;
+    case 'github':
+      imported = import('./themes/github.js');
+      break;
+    case 'kustom-dark':
+      imported = import('./themes/kustom-dark.js');
+      break;
+    case 'kustom-light':
+      imported = import('./themes/kustom-light.js');
+      break;
+    case 'solarized-light':
+      imported = import('./themes/solarized-light.js');
+      break;
+    case 'solarized-dark':
+      imported = import('./themes/solarized-dark.js');
+      break;
+    default:
+      throw 'theme not supported';
+    }
+    imported.then(function(res) {
+      let map = {
+        "atom-one-light": "atomOneLight",
+        "one-dark": "oneDark",
+        "default": "defaultTheme",
+        "github": "github",
+        "kustom-dark": "kustomDark",
+        "kustom-light": "kustomLight",
+        "solarized-light": "solarizedLight",
+        "solarized-dark": "solarizedDark",
+      };
+      this.$.theme.innerHTML = res[map[themeName]].innerHTML;
+    }.bind(this));
+  }
+
   connectedCallback() {
     super.connectedCallback();
     setTimeout(() => {
-      if (this.querySelector('template')) {
-        this._observer = new FlattenedNodesObserver(this.$_content, () => this._updateContent());
-      } else if (this.childNodes.length) {
-        console.error('<code-sample>:', 'content must be provided inside a <template> tag');
-      }
+      let tmp = this.querySelector('template');
+      this._updateContent(tmp);
     }, 1);
   }
 
-  disconnectedCallback() {
-    if (this._observer) {
-      this._observer.disconnect();
-      this._observer = null;
-    }
-  }
-
-  _updateContent() {
+  _updateContent(template) {
     if (this._code) this._code.parentNode.removeChild(this._code);
     if (this._demo) this.$.demo.innerHTML = '';
 
-    const template = this._getCodeTemplate();
-
     if (this.render) {
-      this._demo = this.$_demo.appendChild(
-        document.importNode(template.content, true)
+      this._demo = this.$.demo.appendChild(
+        document.importNode(template, true)
       );
     }
 
     this._highlight(template.innerHTML);
   }
 
-  _getCodeTemplate() {
-    const nodes = FlattenedNodesObserver.getFlattenedNodes(this.$_content);
-    return [].filter.call(nodes, (node) => node.nodeType === Node.ELEMENT_NODE)[0];
-  }
-
   _highlight(str) {
     this._code = document.createElement('code');
     if (this.type) this._code.classList.add(this.type);
     this._code.innerHTML = this._entitize(this._cleanIndentation(str));
-    this.$_code.appendChild(this._code);
+    this.$.code.appendChild(this._code);
     hljs.highlightBlock(this._code);
   }
 
@@ -137,32 +202,26 @@ class CodeSample extends LitElement {
       .replace(/"/g, '&quot;');
   }
 
-  _copyToClipboard() {
+  _copyToClipboard(e) {
+    let target = e.target;
     const tempNode = document.createElement('textarea');
     document.body.appendChild(tempNode);
-    tempNode.value = this._cleanIndentation(this._getCodeTemplate().innerHTML);
+    tempNode.value = this.querySelector('template').innerHTML;
     tempNode.select();
-
     let result = false;
-
     try {
       result = document.execCommand('copy', false);
-      this.$_copyButton.textContent = 'Done';
+      target.innerHTML = 'Done';
     } catch (err) {
       console.error(err);
-      this.$_copyButton.textContent = 'Error';
+      target.innerHTML = 'Error';
     }
-
     tempNode.remove();
-
-    // Return to the copy button after a second.
-    setTimeout(this._resetCopyButtonState.bind(this), 1000);
+    setTimeout(function() {
+      target.innerHTML = 'Copy';
+    }.bind(this), 1000);
 
     return result;
-  }
-
-  _resetCopyButtonState() {
-    this.$_copyButton.textContent = 'Copy';
   }
 }
 
